38535722a7b6b4cf14ac332355249ed1777b7cdf
John B <john@nexys.ch>
2021-09-13 21:41:54 +0200

    re add footer for sha

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Header from "./header";
+import Footer from "./footer";
 
 const Layout = ({ children }: { children: any }) => (
   <div>
@@ -9,6 +10,7 @@ const Layout = ({ children }: { children: any }) => (
     <main className="flex-grow">
       <div className="container mx-auto px-4 py-4">{children} </div>
     </main>
+    <Footer />
   </div>
 );
 
