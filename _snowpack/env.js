6ba4f08598d4afe684e85ef05aee7a943152362e
johnb8005 <johnb8005@users.noreply.github.com>
2021-09-13 20:02:42 +0000

    deploy: b5ed2af1b984565171d125b37f8aa8d4bea15d1d

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,4 +1,4 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "c842e8733cb054d28dd35a2b2f92c778302af03e";
+export const SNOWPACK_PUBLIC_GIT_SHA = "b5ed2af1b984565171d125b37f8aa8d4bea15d1d";
 export const SNOWPACK_PUBLIC_URL = "/markdown-editor";
 export const MODE = "production";
 export const NODE_ENV = "production";
