d32c0052475ad5cc642abbb80d50dc3608a32215
Johan <johan@macair2020>
2022-08-11 14:38:29 +0200

    fix env var

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,10 @@
-export const sha = import.meta.env.SNOWPACK_PUBLIC_GIT_SHA || "arandomsha";
-export const title = import.meta.env.SNOWPACK_PUBLIC_TITLE || "Boilerplate";
-export const basename = import.meta.env.SNOWPACK_PUBLIC_URL;
-
 const ghUrl = "https://github.com/tssapp/markdown-editor";
 
-export const github = { sha: `${ghUrl}/commit/${sha}`, url: ghUrl };
+export const version: string = import.meta.env.VITE_VERSION || "unset_version";
+export const sha: string = import.meta.env.VITE_GIT_SHA || "unset_sha";
+
+export const github = {
+  sha: `${ghUrl}/commit/${sha}`,
+  version: `${ghUrl}/releases/tag/${version}`,
+  url: ghUrl,
+};
