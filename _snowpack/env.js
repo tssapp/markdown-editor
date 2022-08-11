7ed6fd7dcf809b41e40ecbfd5c82f454d55fcc24
johnb8005 <johnb8005@users.noreply.github.com>
2022-08-11 12:06:36 +0000

    deploy: 1189db8a3d165c9667cc6e2ba2e92a995bb40596

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,4 +1,4 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "0fe8d6bad4ea27f6fb64aa55b8d4ae24c4636ba4";
+export const SNOWPACK_PUBLIC_GIT_SHA = "1189db8a3d165c9667cc6e2ba2e92a995bb40596";
 export const SNOWPACK_PUBLIC_URL = "/TSS-BP-Channel-Europe/markdown-editor";
 export const MODE = "production";
 export const NODE_ENV = "production";
